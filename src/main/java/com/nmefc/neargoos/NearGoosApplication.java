ba48efbff9578e433dad026a3e90b975d8971b3b
QuYuan891211 <quyuan891211@163.com>
2019-09-24 16:22:38 +0800

    建立工程

---
+++ src/main/java/com/nmefc/neargoos/NearGoosApplication.java
@@ -0,0 +1,13 @@
+package com.nmefc.neargoos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearGoosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearGoosApplication.class, args);
+	}
+
+}
