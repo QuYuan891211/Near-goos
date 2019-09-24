d3863af96a5b173f057d73681a4401738da08b72
QuYuan891211 <quyuan891211@163.com>
2019-09-24 21:06:53 +0800

    工程架构完成

--- src/main/java/com/nmefc/neargoos/NearGoosApplication.java
+++ src/main/java/com/nmefc/neargoos/NearGoosApplication.java
@@ -1,9 +1,11 @@
 package com.nmefc.neargoos;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.nmefc.neargoos.dao")
 public class NearGoosApplication {
 
 	public static void main(String[] args) {
